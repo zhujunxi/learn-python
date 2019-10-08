bb89fa7bf310bcb797b52678d06e9f48b3d89a87
zhujunxi <531578783@qq.com>
2019-10-08 11:00:34 +0800

    feat: first acquaintance

---
+++ day-1/demo.py
@@ -0,0 +1,19 @@
+"""
+turtle画三角形
+turtle是python内置图形化模板 
+
+https://www.cnblogs.com/chen0307/articles/9645138.html
+"""
+
+import turtle
+
+turtle.pensize(1)
+turtle.pencolor('red')
+
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+turtle.right(120)
+turtle.forward(100)
+
+turtle.mainloop()
